361115720112f1796ec51799cf8f6149f2417d8b
zmrdltl <meenseek5929@naver.com>
2024-11-18 16:04:59 +0900

    feat: 2단계 - 명령어가 등록인 경우 명언과 작가를 표준입력으로 받도록 추가
    Signed-off-by: zmrdltl <meenseek5929@naver.com>

--- app/src/main/java/wise/saying/App.java
+++ app/src/main/java/wise/saying/App.java
@@ -11,11 +11,18 @@ public class App {
         App app = new App();
         app.start();
         Scanner scanner = new Scanner(System.in);
-        
+
         while (true) {
             System.out.print("명령) ");
             String operation = scanner.next();
             
+            if (operation.equals("등록")) {
+                System.out.print("명언 : ");
+                String word = scanner.next();
+
+                System.out.print("작가 : ");
+                String writer = scanner.next();
+            }
             if (operation.equals("종료")) {
                 break;
             }
